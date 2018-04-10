1c4ce1e67a958fb5823c87d5cc879092da0b3177
Can Sözbir <cansozbirr@gmail.com>
2018-04-11 00:25:54 +0300

    10.04.2018 quiz cozumleri eklendi

---
+++ Quizzes/10.04.2018 Lab Quiz 2.1 (A).c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/// A ve B matrislerini toplayıp A nın ilk halini ve toplandıktan sonraki halini bastırınız
+/// ben burada ek olarak matrislere değer atamayı da elle yapmak istedim...
+void cizgi(x);
+void main()
+{
+    int i,j,a,b,toplam[i][j];
+    printf("Satır - Sutun sayisi giriniz\n");
+    scanf("%d%d",&a,&b);
+    a -= 1;b -= 1;
+    int A[a][b],B[a][b];
+
+    for (i=0;i<=a;i++)
+        for (j=0;j<=b;j++)
+            {
+            printf("\nA listesinin %d.satir %d.sutun elemani :",i+1,j+1);
+            scanf("%d",&A[i][j]);
+            printf("\nB listesinin %d.satir %d.sutun elemani :",i+1,j+1);
+            scanf("%d",&B[i][j]);
+            }
+    /// üst kısımda matrisleri kullanıcıya elle tanımlattık
+
+    for (i=0;i<=a;i++){
+        for (j=0;j<=b;j++){
+            toplam[i][j] = A[i][j] + B[i][j];
+            }}
+
+
+    printf("\n\nA ve B dizilerinin toplami  \n\n");
+    for (i=0;i<=a;i++)
+        {
+        for (j=0;j<=b;j++)
+            {
+            if (j==0)
+            printf("|");
+            printf(" %d ",toplam[i][j]);
+            }
+        printf("|\n");
+        }
+
+
+}
+
---
+++ Quizzes/10.04.2018 Lab Quiz 2.2 (A).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/// bir sayının eleman sayısını recursive fonksiyonlar yardımıyla bulunuz
+
+int basamakbul(int n);
+void main()
+{
+    int sayi,x;
+    printf("\nSayiyi Giriniz : ");
+    scanf("%d",&sayi);
+    printf("\n%d sayisi %d basamaklidir\n",sayi,basamakbul(sayi));
+
+}
+
+int basamakbul(int n)
+{
+    if (n<10)
+        return 1;
+    else
+    {
+        return 1+basamakbul(n/10);
+    }
+}
